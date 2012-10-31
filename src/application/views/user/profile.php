c29d1457d6d44f0fc5f30bcda8a2dca6c1216d7e
Adam Eriksson <adamceriksson@gmail.com>
2012-10-31 13:32:42 +0100

    Användarprofilen

--- src/application/views/user/profile.php
+++ src/application/views/user/profile.php
@@ -1,1 +1,33 @@
-Profile
+<div id="BoxOverblick">
+	<div id="innerpicOverblick"><img src="insert relevant link here" height="190" width="190"></div>
+	<div id="Overblickinfo">
+		<ul>
+			<li>Ingen info angiven</li>
+		</ul>	
+		
+
+	</div>
+	<div id="">
+		<div id="innerOverblickname">Exempelnamn</div>
+		<div id="">
+		<a href="http://www.twitter.com" target="_blank"><div id="innerOverblickLink1" ></div></a>
+		<a href="http://www.facebook.com" target="_blank"><div id="innerOverblickLink2"></div></a>
+		</div>
+	</div>
+</div>
+
+<div id="BoxSenAktiv">
+	<div id="innercontainerSenHeader"></div>
+	
+	<div id="innercontainerSen">
+		<div id="Flodet">
+				<ul>
+					<li>Inga Aktiviteter...</li>
+
+				</ul>
+
+
+		</div>
+
+	</div>
+</div>
